0ceb23722f269a4531d834e2e01db7973ab23012
minkymomoattd <67611656+minkymomoattd@users.noreply.github.com>
2025-03-07 22:19:00 -0600

    funcional, falta organizar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function sortNums(x,y,z) {
     let num1=0;
     let num2=0;
     let num3=0;
-    if (x != y != z){
+    if (x != y && x != z && z != x){
         if (x < y){
             num1 = x;
             num2 = y;
@@ -17,14 +17,22 @@ function sortNums(x,y,z) {
         if (num2 < z){
             num3 = z;
         } else {
+            num3 = num2;
             num2 = z;
-            num3 = y;
         }
+        if (num1 < z){
+            alert("ascendente: "+num1+", "+num2+", "+num3);
+            alert("descendente: "+num3+", "+num2+", "+num1);
+        } else {
+            alert("ascendente: "+num2+", "+num1+", "+num3);
+            alert("descendente: "+num3+", "+num1+", "+num2);
+        }
+        
+    } else {
+        alert("error, son iguales");
     }
 
-
-
-    return num1, num2, num3;
+    
 }
 
-alert(sortNums(x,y,z));
+sortNums(x,y,z);
