eb8e352db1d61bab41c274405ccfc8e4b3101972
minkymomoattd <67611656+minkymomoattd@users.noreply.github.com>
2025-03-07 21:58:46 -0600

    revisa si son diferentes los numeros

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ let y = prompt ("enter first number: ");
 let z = prompt ("enter first number: ");
 
 function sortNums(x,y,z) {
-    num1 = x > y? x : y
-    return num1;
+    let num1=0;
+    let num2=0;
+    let num3=0;
+    if (x != y != z){
+        if (x < y){
+            num1 = x;
+            num2 = y;
+        } else {
+            num1 = y;
+            num2 = x;
+        }
+        if (num2 < z){
+            num3 = z;
+        } else {
+            num2 = z;
+            num3 = y;
+        }
+    }
+
+
+
+    return num1, num2, num3;
 }
 
 alert(sortNums(x,y,z));
