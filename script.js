c39f697f4a55bfd861f99692ff9e352372646b59
minkymomoattd <67611656+minkymomoattd@users.noreply.github.com>
2025-03-07 21:54:59 -0600

    funcion sorting

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = prompt ("enter first number: ");
+let y = prompt ("enter first number: ");
+let z = prompt ("enter first number: ");
+
+function sortNums(x,y,z) {
+    num1 = x > y? x : y
+    return num1;
+}
+
+alert(sortNums(x,y,z));
